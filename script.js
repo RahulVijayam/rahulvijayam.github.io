ed996998c5c3c005d442ddaa49d03f1c78cb6e51
Rahul Vijayanagaram <36434065+RahulVijayam@users.noreply.github.com>
2022-01-08 13:03:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){ 
+    $('.Send_data').click(function(e)
+    {
+          
+            if($('input[type=radio][name=item]:checked').length == 0)
+            {
+           // alert("Please select atleast one");
+            return false;
+            }
+            else
+            {
+                var item = $('input[type=radio][name=item]:checked').val(); 
+              //  window.alert("You Selected")
+              
+              window.setTimeout(function(){
+                // do whatever you want to do  
+                $('#loading').html('You Selected : ' + item);   
+                    
+                }, 600);
+
+                $('#loading').html('<br><span class="spinner-border fast"  style="width: 2rem; height: 2rem;color:green;"  role="status"></span>');
+                
+                
+            }      
+        
+          
+        });
+   });
